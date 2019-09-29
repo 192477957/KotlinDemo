052d7b1685bfa61212084905b4309a8c3f546c18
pkroad <192477957@qq.com>
2019-09-29 14:10:04 +0800

    add fun

---
+++ src/main/kotlin/com/pkroad/Special.kt
@@ -0,0 +1,37 @@
+package com.pkroad
+
+fun myFun():String{
+    println("执行了myFun函数")
+    return "这是MyFun函数的返回值"
+}
+
+fun runFunDemo(){
+    myFun()
+    run { myFun() }
+    run({myFun()})
+    run { println("A") }
+}
+fun applyDemo(){
+    val dataList = arrayListOf<String>()
+    dataList.add("A")
+    dataList.add("B")
+    dataList.add("C")
+    println(dataList)
+
+    val dateList = arrayListOf<String>()
+            .apply { add("A")
+                add("B")
+                add("C")}.let { item-> println(item) }
+}
+
+
+fun String.firstChar():String{
+    if (this.length <= 0){
+        return ""
+    }
+    return this[0].toString()
+}
+
+fun testNUll(name:String?):Int{
+    return name?.length?:-1
+}
